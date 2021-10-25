3070ed5d44ac604ac3d36523c2d3ba49279cf0bd
Your Name <you@example.com>
2021-10-25 22:19:14 +0300

    heroku settings

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
+const path = require('path');
 
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
 const errorMiddleware = require('./middlewares/errors');
+
+// Setting up config file
+if (process.env.NODE_ENV !== 'PRODUCTION')
+	require('dotenv').config({ path: 'backend/config/config.env' });
+dotenv.config({ path: 'backend/config/config.env' });
 
 // Setting up config file
 if (process.env.NODE_ENV !== 'PRODUCTION')
@@ -25,12 +31,12 @@ const device = require('./routes/device');
 app.use('/api/v1', auth);
 app.use('/api/v1', device);
 
-// // if (process.env.NODE_ENV === 'PRODUCTION') {
-// app.use(express.static(path.join(__dirname, '../frontend/build')));
+// if (process.env.NODE_ENV === 'PRODUCTION') {
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// app.get('*', (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-// });
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+});
 
 // Middleware to handle errors
 app.use(errorMiddleware);
